fe4aff9abd907ecb579904f229a0c3bc8d2961a8
Bruno Sérvio <bruno.servio@somosicev.com>
2025-03-06 11:02:36 -0300

    Setup and Gitignore

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("olá mundo")
+import pygame
+
+print('Setup start')
+pygame.init()
+print('Setup start')
+window = pygame.display.set_mode(size=(600, 480))
+
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close window
+            quit()  # end pygame
