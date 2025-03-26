1cc73dc760d35531c75b3678621089cdba4beb0f
Bruno Sérvio Barbosa <brunoservio.dev@gmail.com>
2025-03-25 21:13:01 -0300

    Assets folder Menu Image

--- code/Game.py
+++ code/Game.py
@@ -3,12 +3,13 @@
 import pygame
 
 from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600,400))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH,WIN_HEIGHT))
 
     def run(self, ):
        while True:
