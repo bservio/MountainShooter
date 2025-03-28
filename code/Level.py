dcfb1a561adc91a0f11b7bb44ef6b4b22bb1ab77
Bruno Sérvio <bruno.servio@somosicev.com>
2025-03-28 13:55:28 -0300

    Reducing level volume

--- code/Level.py
+++ code/Level.py
@@ -29,6 +29,7 @@ class Level:
     def run(self, ):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
+        pygame.mixer.music.set_volume(0.1)
         clock = pygame.time.Clock()
 
         while True:
